5a67ef49f05f3193c0b71edbf7a4ef7da6f52afd
hixcoder <hixcoder@gmail.com>
2024-01-09 21:30:59 +0100

    start another challenge

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen w-screen">
+      <div>
+        <h1 className="mx-auto mt-8 text-center">HixCoder Wordle</h1>
+      </div>
+    </div>
+  );
+}
