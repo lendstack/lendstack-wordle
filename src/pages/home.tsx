fc939bd7ea2124561f8b3324259cd2eb3c0a051c
AHAMMAM LHOUSSAINE <ahammamlho96@gmail.com>
2024-01-09 12:50:53 +0100

    first commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center  justify-center  h-screen">
+      <h1>Welcome to Lendstack-Wordle</h1>
+      <p>Get ready to test your vocabulary!</p>
+    </div>
+  );
+};
+
+export default Home;
