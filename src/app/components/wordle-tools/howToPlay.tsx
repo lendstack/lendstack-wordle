da376c8861365c8791a438a9d2fafad8c1eb86ab
Ahmed El Bahri <bahri.tanger10@gmail.com>
2024-01-12 21:06:04 +0100

    how to play modal dark mode

--- src/app/components/wordle-tools/howToPlay.tsx
+++ src/app/components/wordle-tools/howToPlay.tsx
@@ -5,7 +5,7 @@ export default function howToPlay(setShowHowTo: any) {
 
 	return (
 	  <div className="absolute mobileWidth h-[calc(100%-68px)] min-h-[1032px] flex items-center justify-center bg-[#00000099]">
-		<div className="p-4  border border-[#121213] rounded shadow h-[600px] w-[500px] bg-[#121213]">
+		<div className="p-4  border border-[#121213] rounded shadow h-[600px] w-[500px] dynamic-bg">
 			<div className="flex justify-end">
 				<button className="mb-4 text-[2rem]"
 					onClick={() => setShowHowTo(false)}
